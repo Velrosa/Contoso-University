e5e582c20159f014ff8775e62f5b05f596e21426
Velrosa <steezie21@hotmail.co.uk>
2022-05-12 21:28:23 +0100

    Added Search Box to Student Index

--- Contoso University/Controllers/StudentController.cs
+++ Contoso University/Controllers/StudentController.cs
@@ -16,7 +16,7 @@ namespace Contoso_University.Controllers
         private SchoolContext db = new SchoolContext();
 
         // GET: Student
-        public ActionResult Index(string sortOrder)
+        public ActionResult Index(string sortOrder, string searchString)
         {
             ViewBag.NameSortParm = String.IsNullOrEmpty(sortOrder) ? "name_desc" : "";
             ViewBag.DateSortParm = sortOrder == "Date" ? "date_desc" : "Date";
@@ -24,6 +24,12 @@ namespace Contoso_University.Controllers
             var students = from s in db.Students
                            select s;
 
+            if (!String.IsNullOrEmpty(searchString))
+            {
+                students = students.Where(s => s.LastName.Contains(searchString) 
+                                            || s.FirstMidName.Contains(searchString));
+            }
+            
             switch (sortOrder)
             {
                 case "name_desc":
